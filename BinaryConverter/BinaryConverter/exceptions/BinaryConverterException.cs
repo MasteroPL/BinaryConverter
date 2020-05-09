8269ca013d5f7f5fa27c70e7a76604f3aceae7a9
jan@sharedmasters.com <jan.przychodniak@gmail.com>
2020-05-09 06:25:34 +0200

    Implementing base exceptions for this library

---
+++ BinaryConverter/BinaryConverter/exceptions/BinaryConverterException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace BinaryConverter.exceptions
+{
+	/// <summary>
+	/// Base exception class for exceptions strictly concerning BinaryConverter and its utilities
+	/// </summary>
+	public class BinaryConverterException : Exception
+	{
+		public BinaryConverterException() : base() { }
+		public BinaryConverterException(string message) : base(message) { }
+		public BinaryConverterException(string message, Exception inner) : base(message, inner) { }
+		public BinaryConverterException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+	}
+}
---
+++ BinaryConverter/BinaryConverter/exceptions/ReadException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace BinaryConverter.exceptions
+{
+	/// <summary>
+	/// Base class for exceptions concerning any failed read operations, strictly concerning BinaryConverter
+	/// </summary>
+	public class ReadException : BinaryConverterException
+	{
+		public ReadException() : base() { }
+		public ReadException(string message) : base(message) { }
+		public ReadException(string message, Exception inner) : base(message, inner) { }
+		public ReadException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+	}
+}
